aafc63c7243e1f2d87c43e4f6e1b1e85c8583cd3
Liweixin <2522048902@qq.com>
2021-02-19 22:08:24 +0800

    完成了ServiceBar组件得开发

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -243,7 +243,8 @@ export default {
               border-top: 1px solid #E5E5E5;
               box-shadow: 0px 7px 6px 0px rgba(0, 0, 0, 0.11);
               z-index: 10;
-              transition: all;
+              transition: all .5s;
+              background-color: #fff;
               }
               .product {
                 position: relative;
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,17 @@
 // 首页 注重复用性
 <template>
   <div>
+    <service-bar></service-bar>
   </div>
 </template>
 
 <script>
+import ServiceBar from './../components/ServiceBar'
 export default {
-  name: 'index'
+  name: 'nav-home',
+  components: {
+    ServiceBar
+  }
 }
 </script>
 
